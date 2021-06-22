fe83c8238de5cd89e5a1dde226cbcf715ec8ecb1
Erikson <erikson.bahr@gmail.com>
2021-06-22 10:56:34 -0300

    Created initial structures.

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,40 @@
 package main
 
-import (
-
-)
-
+type vehicle interface{}
+type car struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+type truck struct {
+	car
+}
+type bike struct {
+	car
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json: "values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNegative int
+	feedbaclNeutral  int
+}
+
+var (
+	vehicleResult map[string]feedbackResult
+	inventory     []vehicle
+)
 
 type rating float32
 
@@ -20,11 +47,9 @@ const (
 )
 
 func init() {
-
-/*
 	inventory = []vehicle{
-		bike{"FTR 1200", "Indian"},
-		bike{"Iron 1200", "Harley"},
+		bike{car{"FTR 1200", "Indian", ""}},
+		bike{car{"Iron 1200", "Harley", ""}},
 		car{"Sonata", "Hyundai", "Sedan"},
 		car{"SantaFe", "Hyundai", "SUV"},
 		car{"Civic", "Honda", "Hatchback"},
@@ -32,17 +57,16 @@ func init() {
 		car{"Mazda6", "Mazda", "Sedan"},
 		car{"CRV", "Honda", "SUV"},
 		car{"Camry", "Toyota", "Sedan"},
-		truck{"F-150", "Ford", "Truck"},
-		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+		truck{car{"F-150", "Ford", "Truck"}},
+		truck{car{"RAM1500", "Dodge", "Truck"}}}
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
